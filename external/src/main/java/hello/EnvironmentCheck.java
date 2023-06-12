b3e026bf819ba721d43b1e9c3072fa185cc15d8a
sungsanghyun <sos9210@naver.com>
2023-06-13 01:08:40 +0900

    설정데이터,우선순위

--- external/src/main/java/hello/EnvironmentCheck.java
+++ external/src/main/java/hello/EnvironmentCheck.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Component;
 public class EnvironmentCheck {
     //스프링에서 제공하는 기능
     //자바시스템옵션(VmOption), 커맨드라인옵션, 설정데이터(파일 ex:properties,yml),OS환경변수 등에서 모두 Key,Value형태로 조회가능하다.
-    //키값이 같은경우 우선순위는 범위가 넓은것보다 좁은것이 더 우선순위를 갖는다.
-    //설정데이터 > 커맨드라인옵션> 자바시스템옵션> OS환경변수
+    //키값이 같은경우 우선순위는 범위가 넓은것보다 좁고 유연한 것이 더 우선순위를 갖는다.
+    //낮음 ------------------------------------ 높음
+    //설정데이터- OS환경변수 - 자바시스템옵션 - 커맨드라인옵션
+    //설정데이터 (내부 jar - 내부 jar 프로필 적용 파일 - jar외부 - jar외부 프로필 적용 파일)
     private final Environment env;
 
     public EnvironmentCheck(Environment env) {
